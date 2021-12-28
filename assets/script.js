92a6f19dc3dcc8d58b3db6ef24587076964b5513
marissann11 <wilhite.marissa@gmail.com>
2021-12-27 20:18:04 -0500

    icon display

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 let submitButtonEl = document.querySelector("#submit-btn");
 let cityInput = document.querySelector("#city");
-//let currentWeather = document.querySelector(".current-weather")
+
 
 submitButtonEl.addEventListener("click", getWeather);
 
@@ -14,13 +14,13 @@ function getWeather(event) {
     })
     .then(function(data) {
         let temp = data.main.temp;
-        //let currentTemp = document.createElement('div');
-        //currentWeather.appendChild(currentTemp);
-        //currentTemp.appendChild(temp);
         let humidity = data.main.humidity;
         let wind = data.wind.speed;
         console.log(temp, humidity, wind);
         getUvi(data.coord.lon, data.coord.lat);
+        console.log(data);
+        let iconUrl = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+        $("#weather-icon").attr('src', iconUrl)
     })
     function getUvi(lon, lat) {
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=d91f911bcf2c0f925fb6535547a5ddc9`)
@@ -30,6 +30,17 @@ function getWeather(event) {
         .then(function(data) {
             let uvi = data.current.uvi;
             console.log(uvi);
+        });
+        getFiveDay();
+    }
+    function getFiveDay() {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=d91f911bcf2c0f925fb6535547a5ddc9`)
+        .then(function(res) {
+            return res.json()
+        })
+        .then(function(data) {
+            // loop through array 5 times to get temp, wind, humidity, and icon for next 5 days
+            console.log(data)
         })
     }
 }
