37b70be1f6e4a6a9fb3e32c2f3c185cb4c997f06
marissann11 <wilhite.marissa@gmail.com>
2021-12-31 19:23:38 -0500

    added forecast function

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,15 @@
 let submitButtonEl = document.querySelector("#submit-btn");
 let cityInput = document.querySelector("#city");
 let cardZone = document.querySelector("#weather-card");
+let forecastZone = document.querySelector("#forecast-card");
 
 submitButtonEl.addEventListener("click", getWeather);
 
 async function getWeather(event) {
+  let weatherContainerEl = document.querySelector(".weather-container");
+  if (weatherContainerEl) {
+    weatherContainerEl.remove();
+  }
   event.preventDefault();
   let cityName = document.querySelector("#city").value;
   const response = await fetch(
@@ -14,53 +19,81 @@ async function getWeather(event) {
 
   let iconUrl = `https://openweathermap.org/img/w/${res.weather[0].icon}.png`;
 
-  const cardContainer = document.createElement('div');
-  const cardBody = document.createElement('div');
-  const cardTitle = document.createElement('h4');
-  const cardIcon = document.createElement('img');
-  const cardTemp = document.createElement('p');
-  const cardWind = document.createElement('p');
-  const cardHumidity = document.createElement('p');
+  const cardContainer = document.createElement("div");
+  const cardBody = document.createElement("div");
+  const cardTitle = document.createElement("h4");
+  const cardIcon = document.createElement("img");
+  const cardTemp = document.createElement("p");
+  const cardWind = document.createElement("p");
+  const cardHumidity = document.createElement("p");
 
-  cardContainer.setAttribute('class', 'card');
-  cardBody.setAttribute('class', 'card-body');
-  cardBody.setAttribute('id', 'card-body');
-  cardTitle.setAttribute('class', 'card-title');
-  cardIcon.setAttribute('src', iconUrl);
-  cardTemp.setAttribute('class', 'card-text');
-  cardWind.setAttribute('class', 'card-text');
-  cardHumidity.setAttribute('class', 'card-text');
+  cardContainer.setAttribute("class", "card weather-container");
+  cardBody.setAttribute("class", "card-body");
+  cardBody.setAttribute("id", "card-body");
+  cardTitle.setAttribute("class", "card-title");
+  cardIcon.setAttribute("src", iconUrl);
+  cardTemp.setAttribute("class", "card-text");
+  cardWind.setAttribute("class", "card-text");
+  cardHumidity.setAttribute("class", "card-text");
 
   cardTitle.textContent = cityName;
-  cardTemp.textContent = `Temp: ${res.main.temp} °F`
-  cardWind.textContent = `Wind: ${res.wind.speed} MPH`
-  cardHumidity.textContent = `Humidity: ${res.main.humidity} %`
+  cardTemp.textContent = `Temp: ${res.main.temp} °F`;
+  cardWind.textContent = `Wind: ${res.wind.speed} MPH`;
+  cardHumidity.textContent = `Humidity: ${res.main.humidity} %`;
 
   cardContainer.append(cardBody);
   cardTitle.append(cardIcon);
   cardBody.append(cardTitle, cardTemp, cardWind, cardHumidity);
   cardZone.append(cardContainer);
-  
+
   getUvi(res.coord.lon, res.coord.lat);
-  getFiveDay(cityName);
+  getFiveDay(res.coord.lon, res.coord.lat);
 }
 async function getUvi(lon, lat) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=d91f911bcf2c0f925fb6535547a5ddc9`
   );
   const res = await response.json();
-  
-  const cardUvi = document.createElement('p');
-  cardUvi.setAttribute('class', 'card-text');
-  cardUvi.textContent = `UVI: ${res.current.uvi}`
+
+  const cardUvi = document.createElement("p");
+  cardUvi.setAttribute("class", "card-text");
+  cardUvi.textContent = `UV Index: ${res.current.uvi}`;
   let cardBody = document.querySelector("#card-body");
   cardBody.append(cardUvi);
 }
-async function getFiveDay(cityName) {
+async function getFiveDay(lon, lat) {
+  let forecastContainerEl = document.querySelector(".forecast-container");
+  if (forecastContainerEl) {
+    forecastContainerEl.remove();
+  }
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=d91f911bcf2c0f925fb6535547a5ddc9`
-  )
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`
+  );
   const res = await response.json();
-    console.log(res);
-}
 
+  for (i = 0; i < 5; i++) {
+    let iconUrl = `https://openweathermap.org/img/w/${res.daily[i].weather[0].icon}.png`;
+
+    const dailyContainer = document.createElement("div");
+    const dailyBody = document.createElement("div");
+    const dailyIcon = document.createElement("img");
+    const dailyTemp = document.createElement("p");
+    const dailyWind = document.createElement("p");
+    const dailyHumidity = document.createElement("p");
+
+    dailyContainer.setAttribute("class", "card forecast-container");
+    dailyBody.setAttribute("class", "card-body");
+    dailyIcon.setAttribute("src", iconUrl);
+    dailyTemp.setAttribute("class", "card-text");
+    dailyWind.setAttribute("class", "card-text");
+    dailyHumidity.setAttribute("class", "card-text");
+
+    dailyTemp.textContent = `Temp: ${res.daily[i].temp.day} °F`
+    dailyWind.textContent = `Wind: ${res.daily[i].wind_speed} MPH`
+    dailyHumidity.textContent = `Humidity: ${res.daily[i].humidity} %`
+
+    dailyContainer.append(dailyBody);
+    dailyBody.append(dailyIcon, dailyTemp, dailyWind, dailyHumidity);
+    forecastZone.append(dailyContainer);
+  }
+}
