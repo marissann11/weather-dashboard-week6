7341c83abd1a17fadd80d3c73c0c81cb76086f35
marissann11 <wilhite.marissa@gmail.com>
2022-01-05 14:02:21 -0500

    added event listeners and refactored getWeather function

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,26 @@
+$(document).ready(function() {
+
 let submitButtonEl = document.querySelector("#submit-btn");
 let cityInput = document.querySelector("#city");
 let cardZone = document.querySelector("#weather-card");
 let forecastZone = document.querySelector("#forecast-card");
 let formZone = document.querySelector("#form-card");
-let searchedCities = [];
 
-submitButtonEl.addEventListener("click", getWeather);
+// Function called when user manually searches for a city name that calls getWeather
+function getCity(e) {
+  e.preventDefault();
+  let cityName = cityInput.value;
+  getWeather(cityName)
+}
 
-async function getWeather(event) {
+submitButtonEl.addEventListener("click", getCity);
+
+async function getWeather(cityName) {
+  // Because all elements are dynamically created, this makes sure they are removed after new searches
   let weatherContainerEl = document.querySelector(".weather-container");
   if (weatherContainerEl) {
     weatherContainerEl.remove();
   }
-  event.preventDefault();
-  let cityName = document.querySelector("#city").value;
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?appid=d91f911bcf2c0f925fb6535547a5ddc9&q=${cityName}&units=imperial`
   );
@@ -22,6 +29,7 @@ async function getWeather(event) {
   let iconUrl = `https://openweathermap.org/img/w/${res.weather[0].icon}.png`;
   let currentDate = res.dt * 1000;
 
+  // Dynamically creates main weather container with current weather of city
   const cardContainer = document.createElement("div");
   const cardBody = document.createElement("div");
   const cardTitle = document.createElement("h4");
@@ -55,6 +63,7 @@ async function getWeather(event) {
   getFiveDay(res.coord.lon, res.coord.lat);
   saveSearch(cityName);
 }
+// A separate fetch was needed using the lon and lat coordinates to retrieve the uvi 
 async function getUvi(lon, lat) {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=d91f911bcf2c0f925fb6535547a5ddc9&units=imperial`
@@ -69,6 +78,7 @@ async function getUvi(lon, lat) {
 
   $("#card-body").append(cardUvi);
 
+  // Adds classes based on uvi severity 
   if (res.current.uvi <= 3) {
     $("#uvi").addClass("bg-success p-2 text-light rounded");
   } else if (res.current.uvi < 7) {
@@ -77,7 +87,9 @@ async function getUvi(lon, lat) {
     $("#uvi").addClass("bg-danger p-2 text-light rounded");
   }
 }
+// Separate fetch using lon and lat for 5 day forecast
 async function getFiveDay(lon, lat) {
+  // Removes current forecast (if applicable) before dynamically creating the new one
   if ($(".forecast-container")) {
     $(".forecast-container").remove();
   }
@@ -119,11 +131,13 @@ async function getFiveDay(lon, lat) {
 function saveSearch(cityName) {
   let searchedCities = JSON.parse(localStorage.getItem("cityname"));
 
+  // If there are no cities in local storage, a new array is created. Unshift is to move city names to beginning rather than end
   if (searchedCities == null) {
     searchedCities = [];
     searchedCities.unshift(cityName);
     localStorage.setItem("cityname", JSON.stringify(searchedCities));
   }
+  // User can only have max 8 previous buttons saved for quick searches
   if (searchedCities.length > 7) {
     searchedCities.pop();
   }
@@ -139,17 +153,24 @@ function displayHistory() {
     for (i = 0; i < searchedCities.length; i++) {
       const previousBtn = document.createElement("button");
 
-      previousBtn.setAttribute("class", "btn");
-      previousBtn.setAttribute("id", "previous-btn");
+      previousBtn.setAttribute("class", "btn previous-btn");
+
+      previousBtn.setAttribute("value", searchedCities[i])
 
       previousBtn.textContent = searchedCities[i];
 
+      previousBtn.onclick = checkHistory
+
       formZone.append(previousBtn);
     }
-    $("#previous-btn").on("click", function () {
-      let city = document.querySelector("#previous-btn").innerText;
-      console.log(city);
-    });
   }
 };
-$(document).ready(displayHistory);
+// Function called with click of a previous city button which redirects to getWeather function 
+function checkHistory() {
+  const cityName = this.value
+  getWeather(cityName)
+}
+
+displayHistory();
+});
+
